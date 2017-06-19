a7dc8ee15e74823507f3c5c0c6d99bc8598f2690
俞诚恺 <zx597813039@gmail.com>
2017-06-19 10:05:23 +0800

    no message

---
+++ TypeScript/base.js
@@ -0,0 +1,8 @@
+function sayHello(person) {
+    return "Hello, " + person;
+}
+console.log(sayHello('yck'));
+var isDone = false;
+function alertName() {
+    alert('My name is YCK');
+}
---
+++ TypeScript/base.ts
@@ -0,0 +1,9 @@
+function sayHello(person: string) {
+    return `Hello, ${person}` 
+}
+console.log(sayHello('yck'));
+let isDone: boolean = false
+
+function alertName(): void {
+    alert('My name is YCK')
+}
