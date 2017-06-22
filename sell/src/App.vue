dd6d84e2eb9b59a1eb000f5843cfdf3e1d1b285d
俞诚恺 <zx597813039@gmail.com>
2017-06-22 17:57:00 +0800

    Head 组件拆分初始化

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <v-header></v-header>
+    <div id="main">
+      i am main
+    </div>
+    <div id="footer">
+      i am footer
+    </div>
   </div>
 </template>
 
 <script>
+
+import header from './components/header/header'
+
 export default {
-  name: 'app'
+  components: {
+    'v-header': header
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
+
---
+++ sell/src/components/header/header.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <div id="header">
+      i am header
+    </div>
+
+</template>
+
+<script>
+
+export default {}
+
+</script>
+
+<style>
+
+</style>
