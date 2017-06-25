2ad0f9d337ba8b51968a56d71aeae7f268112bbd
yuchengkai <zx597813039@gmail.com>
2017-06-25 22:35:37 +0800

    webpack

--- Study-Webpack/webpack.config.js
+++ Study-Webpack/webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
-    app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build')
-}
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+};
 
 module.exports = {
-    entry: {
-        app: PATHS.app,
-    },
-    output: {
-        path: PATHS.build,
-        filename: '[name].js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Webpack Demo!'
-        })
-    ]
-}
+  entry: {
+    app: PATHS.app,
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Demo!',
+    }),
+  ],
+};
