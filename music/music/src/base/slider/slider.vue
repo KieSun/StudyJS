0ca4b61c9b3c93cc5fd8a49a42d503adac2b956d
俞诚恺 <zx597813039@gmail.com>
2017-07-17 18:07:57 +0800

    Music

---
+++ music/music/src/base/slider/slider.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot></slot>
+    </div>
+    <div class="dot-wrapper">
+      <span class="dot" v-for="(item, index) in dotArray" :key="index" :class="{active: currentIndex===index}"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+import { addClass } from 'api/dom'
+
+export default {
+  data() {
+    return {
+      dotArray: [],
+      currentIndex: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.setSliderWidth()
+      this.initScroll()
+    }, 20)
+  },
+  methods: {
+    setSliderWidth() {
+      let slider = this.$refs.sliderGroup
+      let children = slider.children
+      this.getDotArray(children.length)
+      let width = 0
+      let sliderWidth = this.$refs.slider.clientWidth
+
+      for (let index = 0; index < children.length; index++) {
+        let ele = children[index]
+        addClass(ele, 'slider-item')
+        ele.style.width = sliderWidth + 'px'
+        width += sliderWidth
+      }
+      width += sliderWidth * 2
+      slider.style.width = width + 'px'
+    },
+    initScroll() {
+      this.slider = new BScroll(this.$refs.slider, {
+        scrollX: true,
+        scrollY: false,
+        snapLoop: true,
+        snapSpeed: 200,
+        momentum: false,
+        snap: true
+      })
+
+      this.slider.on('scrollEnd', () => {
+        this.currentIndex = this.slider.getCurrentPage().pageX - 1
+      })
+    },
+    getDotArray(length) {
+      this.dotArray = new Array(length)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~common/css/variable';
+
+.slider {
+  min-height: 1px;
+  position: relative;
+  .slider-group {
+    position: relative;
+    overflow: hidden;
+    white-space: nowrap;
+    .slider-item {
+      float: left;
+      box-sizing: border-box;
+      overflow: hidden;
+      text-align: center;
+      a {
+        text-decoration: none;
+        width: 100%;
+        overflow: hidden;
+        display: block;
+      }
+      img {
+        width: 100%;
+        display: block;
+      }
+    }
+  }
+  .dot-wrapper {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 12px;
+    text-align: center;
+    .dot {
+      display: inline-block;
+      margin: 0 4px;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background: $color-text-l;
+      &.active {
+        width: 20px;
+        border-radius: 5px;
+        background: $color-text-ll;
+      }
+    }
+  }
+}
+</style>
--- music/music/src/components/suggest/suggest.vue
+++ music/music/src/components/suggest/suggest.vue
@@ -1,11 +1,45 @@
 <template>
   <div class="suggest">
-      推荐
+    <div class="slider-wrapper" v-if="recommends.length">
+      <slider>
+        <div v-for="(item, index) in recommends" :key="index">
+          <a href="item.linkUrl">
+            <img :src="item.picUrl">
+          </a>
+        </div>
+      </slider>
+    </div>
+  
   </div>
 </template>
 
 <script>
-export default {}
+import Slider from 'base/slider/slider'
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
+
+export default {
+  data() {
+    return {
+      recommends: []
+    }
+  },
+  components: {
+    Slider
+  },
+  methods: {
+    getData() {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    }
+  },
+  created() {
+    this.getData()
+  }
+}
 </script>
 
 <style lang="scss">
